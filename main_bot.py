bc633cc5099192ee4417ff6560cd8c5b5fe49847
Davinchi7788 <seyranmatos@icloud.com>
2026-09-20 07:09:14 +0400

    Update main_bot.py

--- main_bot.py
+++ main_bot.py
@@ -186,7 +186,9 @@ async def main_async():
     await site.start()
     
     # Պահում ենք բոտը ակտիվ վիճակում
+        await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main_async())
